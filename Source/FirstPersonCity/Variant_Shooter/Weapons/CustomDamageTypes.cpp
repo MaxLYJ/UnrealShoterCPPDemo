a9d4070563cb5b4a2088d7c0d728490453569811
MaXLYJ <bobchaoliu@gmail.com>
2025-09-30 20:57:24 +0800

    Submit material. Submit all 8 types of damage type.

--- Source/FirstPersonCity/Variant_Shooter/Weapons/CustomDamageTypes.cpp
+++ Source/FirstPersonCity/Variant_Shooter/Weapons/CustomDamageTypes.cpp
@@ -52,3 +52,83 @@ UExplosiveDamageType::UExplosiveDamageType()
 	DestructibleDamageSpreadScale = 5.0f;
 	DamageFalloff = 2.0f; // Quadratic falloff for explosions
 }
+
+UJoyDamageType::UJoyDamageType()
+{
+	// Joy damage has uplifting effect with moderate impulse
+	bScaleMomentumByMass = true;
+	DamageImpulse = 400.0f;
+	DestructibleImpulse = 400.0f;
+	DestructibleDamageSpreadScale = 1.5f;
+	DamageFalloff = 1.0f;
+}
+
+UTrustDamageType::UTrustDamageType()
+{
+	// Trust damage has calming effect with low impulse
+	bScaleMomentumByMass = true;
+	DamageImpulse = 150.0f;
+	DestructibleImpulse = 150.0f;
+	DestructibleDamageSpreadScale = 1.0f;
+	DamageFalloff = 1.0f;
+}
+
+UTerrorDamageType::UTerrorDamageType()
+{
+	// Terror damage has frightening effect with high impulse
+	bScaleMomentumByMass = false;
+	DamageImpulse = 1000.0f;
+	DestructibleImpulse = 1000.0f;
+	DestructibleDamageSpreadScale = 3.0f;
+	DamageFalloff = 1.5f;
+}
+
+UAmazingDamageType::UAmazingDamageType()
+{
+	// Amazing damage has stunning effect with moderate impulse
+	bScaleMomentumByMass = true;
+	DamageImpulse = 600.0f;
+	DestructibleImpulse = 600.0f;
+	DestructibleDamageSpreadScale = 2.0f;
+	DamageFalloff = 1.0f;
+}
+
+USadnessDamageType::USadnessDamageType()
+{
+	// Sadness damage has depressing effect with low impulse
+	bScaleMomentumByMass = true;
+	DamageImpulse = 100.0f;
+	DestructibleImpulse = 100.0f;
+	DestructibleDamageSpreadScale = 0.8f;
+	DamageFalloff = 1.0f;
+}
+
+ULoathingDamageType::ULoathingDamageType()
+{
+	// Loathing damage has disgusting effect with moderate impulse
+	bScaleMomentumByMass = true;
+	DamageImpulse = 500.0f;
+	DestructibleImpulse = 500.0f;
+	DestructibleDamageSpreadScale = 1.8f;
+	DamageFalloff = 1.2f;
+}
+
+URageDamageType::URageDamageType()
+{
+	// Rage damage has violent effect with very high impulse
+	bScaleMomentumByMass = false;
+	DamageImpulse = 1300.0f;
+	DestructibleImpulse = 1300.0f;
+	DestructibleDamageSpreadScale = 4.0f;
+	DamageFalloff = 1.8f;
+}
+
+UExpectDamageType::UExpectDamageType()
+{
+	// Expect damage has anticipatory effect with variable impulse
+	bScaleMomentumByMass = true;
+	DamageImpulse = 300.0f;
+	DestructibleImpulse = 300.0f;
+	DestructibleDamageSpreadScale = 1.2f;
+	DamageFalloff = 1.0f;
+}
--- Source/FirstPersonCity/Variant_Shooter/Weapons/CustomDamageTypes.h
+++ Source/FirstPersonCity/Variant_Shooter/Weapons/CustomDamageTypes.h
@@ -65,3 +65,99 @@ class FIRSTPERSONCITY_API UExplosiveDamageType : public UDamageType
 public:
 	UExplosiveDamageType();
 };
+
+/**
+ * Joy damage type for projectiles that deal joy-based emotional damage
+ */
+UCLASS(BlueprintType)
+class FIRSTPERSONCITY_API UJoyDamageType : public UDamageType
+{
+	GENERATED_BODY()
+
+public:
+	UJoyDamageType();
+};
+
+/**
+ * Trust damage type for projectiles that deal trust-based emotional damage
+ */
+UCLASS(BlueprintType)
+class FIRSTPERSONCITY_API UTrustDamageType : public UDamageType
+{
+	GENERATED_BODY()
+
+public:
+	UTrustDamageType();
+};
+
+/**
+ * Terror damage type for projectiles that deal terror-based emotional damage
+ */
+UCLASS(BlueprintType)
+class FIRSTPERSONCITY_API UTerrorDamageType : public UDamageType
+{
+	GENERATED_BODY()
+
+public:
+	UTerrorDamageType();
+};
+
+/**
+ * Amazing damage type for projectiles that deal amazement-based emotional damage
+ */
+UCLASS(BlueprintType)
+class FIRSTPERSONCITY_API UAmazingDamageType : public UDamageType
+{
+	GENERATED_BODY()
+
+public:
+	UAmazingDamageType();
+};
+
+/**
+ * Sadness damage type for projectiles that deal sadness-based emotional damage
+ */
+UCLASS(BlueprintType)
+class FIRSTPERSONCITY_API USadnessDamageType : public UDamageType
+{
+	GENERATED_BODY()
+
+public:
+	USadnessDamageType();
+};
+
+/**
+ * Loathing damage type for projectiles that deal loathing-based emotional damage
+ */
+UCLASS(BlueprintType)
+class FIRSTPERSONCITY_API ULoathingDamageType : public UDamageType
+{
+	GENERATED_BODY()
+
+public:
+	ULoathingDamageType();
+};
+
+/**
+ * Rage damage type for projectiles that deal rage-based emotional damage
+ */
+UCLASS(BlueprintType)
+class FIRSTPERSONCITY_API URageDamageType : public UDamageType
+{
+	GENERATED_BODY()
+
+public:
+	URageDamageType();
+};
+
+/**
+ * Expect damage type for projectiles that deal expectation-based emotional damage
+ */
+UCLASS(BlueprintType)
+class FIRSTPERSONCITY_API UExpectDamageType : public UDamageType
+{
+	GENERATED_BODY()
+
+public:
+	UExpectDamageType();
+};
